01ac5220885cb2e0e76aea47a71f5d93aeb8ca6f
bipmaz <118344639+bipmaz@users.noreply.github.com>
2023-04-02 06:51:51 +0300

    End

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-import cesar as ces
+import interface as int
 
-try:
-    user_key = int(input('Введите значение ключа: если '
-         'нужно сдвинуть вправо - положительное число, влево - отрицательное\n'))
-except ValueError:
-    print ('Некорректный ввод! Введите число!')
-user_file_key = input('Введите название для файла, в котором будет храниться ключ\n')
-with open(user_file_key, 'w', encoding="utf-8") as f1:
-    f1.write(str(user_key))
-object_shifvr = input('Введите путь до файла, который нужно зашифровать\n')
-object_shifvr_2 = input('Введите название файла, в котором будет зашифрованный текст\n')
-with open(object_shifvr_2, 'w', encoding="utf-8") as f3:
-    with open(object_shifvr, 'r', encoding="utf-8") as f2:
-        for line in f2:
-            for symbol in line:
-                if symbol.isalpha() == True:
-                    pt = ces.Cesar(ord(symbol), user_key)
-                    f3.write(chr(ces.Cesar.STR(pt)))
-                else:
-                    f3.write(symbol)
+
+if __name__ == '__main__':
+    int.interface()
